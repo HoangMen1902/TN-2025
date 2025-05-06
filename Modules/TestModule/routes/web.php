c0b49ea65c0f44274e007415e10927ba346ba649
7CO <hoangmen9918@gmail.com>
2025-05-06 21:38:04 +0700

    Test Module Và setup scss

---
+++ Modules/TestModule/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\TestModule\Http\Controllers\TestModuleController;
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('testmodules', TestModuleController::class)->names('testmodule');
+});
+
+Route::get('/test', [TestModuleController::class, 'index']);
