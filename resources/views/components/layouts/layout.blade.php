40dd4aaf00befeb2bf41c5d4fdcaecace33bb72d
7CO <hoangmen9918@gmail.com>
2025-05-07 14:10:18 +0700

    [Jquery] Added jquery to layout

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -15,5 +15,6 @@
 
     <!-- script -->
     {{ $scripts ?? ''}}
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
 </body>
 </html>
