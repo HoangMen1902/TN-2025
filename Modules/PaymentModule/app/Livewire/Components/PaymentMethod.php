b12fc0c0469c2acafbef86b569ae99b9532ada51
7CO <hoangmen9918@gmail.com>
2025-07-09 15:18:25 +0700

    [Client][BE] - Tách payment method ra thành livewire

---
+++ Modules/PaymentModule/app/Livewire/Components/PaymentMethod.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\PaymentModule\Livewire\Components;
+
+use Livewire\Component;
+
+class PaymentMethod extends Component
+{
+    public function render()
+    {
+        return view('paymentmodule::livewire.components.payment-method');
+    }
+}
