8a2284f76ae8b6070aedb1901ee64dfd872d8f26
NhutTien <dangnhuttien1004@gmail.com>
2025-07-24 14:11:08 +0700

    [migration] - thêm cột is_paid và is_locked

---
+++ database/migrations/2025_05_12_179100_add_is_paid_to_ebook_payment_details_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('ebook_payment_details', function (Blueprint $table) {
+            $table->boolean('is_paid')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('ebook_payment_details', function (Blueprint $table) {
+            $table->dropColumn('is_paid');
+        });
+    }
+};
