a526115d0754cbc7cd187ca3415f9a5dfbc84e2b
7CO <hoangmen9918@gmail.com>
2025-07-24 14:34:09 +0700

    [Test Push Vps]

--- Modules/HomeModule/resources/views/livewire/components/publisher-product.blade.php
+++ Modules/HomeModule/resources/views/livewire/components/publisher-product.blade.php
@@ -28,14 +28,19 @@
                                 $price = $sku->price ?? 0;
                                 $hasSale = $sale && $sale < $price;
                                 $percent = $hasSale ? round(($price - $sale) / $price * 100) : 0;
+                                $thumbnailPath = $product->thumbnail ?? null;
+
                             @endphp
-                            <a href="/chi-tiet/{{ $product->slug }}" class="block group h-full">
+                                <a href="{{ $product->is_ebook ? route('ebooks.show', $product->id) : url('/chi-tiet/' . $product->slug) }}">
                                 <div
                                     class="flex flex-col justify-between h-full bg-white border border-gray-100 rounded-xl overflow-hidden hover:shadow-md transition-all duration-300">
                                     <div>
                                         <div class="w-full overflow-hidden">
-                                            <img src="{{ asset('storage/' . $product->thumbnail) }}" alt="{{ $product->name }}"
+                                            <img src="{{ asset('storage/' . $thumbnailPath) }}"
                                                 class="w-full aspect-[4/3] object-cover group-hover:scale-105 transition-transform duration-300">
+                                            @if($product->is_ebook)
+                                                <span class="absolute top-1 left-1 bg-blue-500 text-white text-xs px-2 py-1 rounded">Ebook</span>
+                                            @endif
                                         </div>
                                         <div class="p-3">
                                             <h3 class="text-sm font-semibold text-gray-800 line-clamp-2 min-h-[2.5rem]">
