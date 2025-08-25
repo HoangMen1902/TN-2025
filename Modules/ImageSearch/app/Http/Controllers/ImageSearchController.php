7e0b24f2be44982ee7c077777148108aa352e183
7CO <hoangmen9918@gmail.com>
2025-08-25 21:32:03 +0700

    LOg - 7

--- Modules/ImageSearch/app/Http/Controllers/ImageSearchController.php
+++ Modules/ImageSearch/app/Http/Controllers/ImageSearchController.php
@@ -86,7 +86,10 @@ class ImageSearchController extends Controller
             return redirect('/chi-tiet/' . $sku->product->slug);
         }
 
-        Log::info('Sku Status: ' . ($sku ?? "") . ' ' . ($sku?->product) . ' ' . ($sku?->product?->product_status === 'active'));
+        Log::info($sku);
+        Log::info($sku?->product?->product_status === 'active');
+        Log::info($sku?->product);
+
         return back()->with('error', 'Không tìm thấy sản phẩm nào tương tự :((');
     }
 
