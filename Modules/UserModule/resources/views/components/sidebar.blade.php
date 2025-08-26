854df92133f8635e7dae2b8aebd9b4c087a4adcc
NguyenHoaiBaoNe <nguyenhoaibao12c4@gmail.com>
2025-08-26 17:57:07 +0700

    [Fix] Bat loi duong dan, them route cho sidebar

--- Modules/UserModule/resources/views/components/sidebar.blade.php
+++ Modules/UserModule/resources/views/components/sidebar.blade.php
@@ -54,10 +54,6 @@
                 <div class="py-2 text-gray-700 cursor-pointer hover:text-blue-600">
                     <a href="{{ route('change-password') }}">Đổi Mật Khẩu</a>
                 </div>
-
-                <div class="py-2 text-gray-700 cursor-pointer hover:text-blue-600">
-                    <a href="#">Những Thiết Lập Riêng Tư</a>
-                </div>
             </div>
         </div>
 
@@ -85,6 +81,12 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
             </svg>
             <a href="/quan-ly-ebook"> Quản lý ebook</a>
+        </div>
+        <div class="flex items-center py-2 px-1 text-gray-700 hover:text-red-600">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+            </svg>
+            <a href="/quan-ly-ebook">Đăng xuất</a>
         </div>
     </div>
 </div>
