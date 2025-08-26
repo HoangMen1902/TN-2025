7c27e718eb782a2e5e152493b4d388aa25622f10
7CO <hoangmen9918@gmail.com>
2025-08-26 19:02:24 +0700

    Sharp image

--- Modules/ImageSearch/app/Http/Controllers/ImageSearchController.php
+++ Modules/ImageSearch/app/Http/Controllers/ImageSearchController.php
@@ -53,7 +53,7 @@ class ImageSearchController extends Controller
         Storage::disk('public')->put("temp/$filename", base64_decode($base64));
         $inputImage = storage_path("app/public/temp/$filename");
         Log::info('Uploaded Image');
-        Image::load($inputImage)->fit(Fit::Crop, 224, 224)->sharpen(50)->save($inputImage);
+        Image::load($inputImage)->sharpen(50)->save($inputImage);
 
         Log::info('Resized Image');
         $isWin = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
